a67c3b15abffa4e135c1350654adbf69999c5dc8
ashuchaitu2026 <ashuchaitu2026@gmail.com>
2026-06-27 14:02:39 +0530

    Update Config.js

--- js/Config.js
+++ js/Config.js
@@ -6,3 +6,24 @@ const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDato8H7RegmOVwDmdkFtgLgyRe1Y_sg6Q",
+  authDomain: "ashuai-481f3.firebaseapp.com",
+  projectId: "ashuai-481f3",
+  storageBucket: "ashuai-481f3.firebasestorage.app",
+  messagingSenderId: "239643358154",
+  appId: "1:239643358154:web:2a275d65b937a98a695533",
+  measurementId: "G-0DHZSH9B4V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
