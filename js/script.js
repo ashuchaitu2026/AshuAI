94033dc9e53cc31d3f338352c2ee4e0e28064202
ashuchaitu2026 <ashuchaitu2026@gmail.com>
2026-06-27 13:07:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
+async function loadTrainingData() {
 
+    const { data, error } = await supabaseClient
+        .from("trainingdata")
+        .select("*")
+        .order("id", { ascending: true });
+
+    const container = document.getElementById("training-list");
+
+    if (error) {
+
+        console.error(error);
+
+        container.innerHTML = `
+            <div class="card">
+                <h3>Error Loading Data</h3>
+                <p>${error.message}</p>
+            </div>
+        `;
+
+        return;
+    }
+
+    container.innerHTML = "";
+
+    data.forEach(item => {
+
+        container.innerHTML += `
+            <div class="card">
+
+                <h3>${item.title}</h3>
+
+                <p>${item.desc || ""}</p>
+
+                <div class="tech">
+                    ${item.tech || ""}
+                </div>
+
+                <br>
+
+                <a
+                    class="btn"
+                    href="${item.link}"
+                    target="_blank"
+                >
+                    Open Training
+                </a>
+
+            </div>
+        `;
+
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadTrainingData();
+
+});
