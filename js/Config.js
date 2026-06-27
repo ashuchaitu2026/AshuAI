0cd5b0d1d3885f2414a7b028a20182fb3c01cd2c
ashuchaitu2026 <ashuchaitu2026@gmail.com>
2026-06-27 22:16:42 +0530

    Update Config.js

--- js/Config.js
+++ js/Config.js
@@ -1,11 +1,3 @@
-const SUPABASE_URL = "https://kaojyzzqsfmpxjjnbros.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_j9BIi8baQKy4cMP4niiuEA_921Moqtp";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
 // =========================================
 // SUPABASE CONFIGURATION
 // =========================================
