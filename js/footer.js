88d892af059ab7c694838fc5cc4db8db98829bc1
ashuchaitu2026 <ashuchaitu2026@gmail.com>
2026-06-28 13:49:21 +0530

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,11 @@
+async function loadFooter() {
 
+    const response = await fetch("components/footer.html");
+
+    const html = await response.text();
+
+    document.getElementById("footer-placeholder").innerHTML = html;
+
+}
+
+loadFooter();
